ac95dde77c81b9337fab95d9af4260813143677d
AdityaNachiappan <35346621+AdityaNachiappan@users.noreply.github.com>
2018-02-18 19:33:24 -0800

    Update flask_brevets.py

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -53,12 +53,17 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    bDistance = request.args.get('bDistance', type = float)
+    bTime = request.args.get('bTime', type = float)
+    bDate = request.args.get('bDate', type = float)
+    
+    
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
-    # FIXME: These probably aren't the right open and close times
-    # and brevets may be longer than 200km
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat)
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat)
+    #Now takes correct distance/time
+   
+    open_time = acp_times.open_time(km, bDistance, bTime)
+    close_time = acp_times.close_time(km, bDistance, bTime)
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
